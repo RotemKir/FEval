f3af7c9279fd909b591ce2dc800b762cf8fd1ec1
Rotem Kirshenbaum <rotemkir@gmail.com>
2018-11-23 08:28:16 +0200

    Added saving to file for data set inspection

--- FEval/Inspections/DataSetInspector.fs
+++ FEval/Inspections/DataSetInspector.fs
@@ -5,18 +5,22 @@ module DataSetInspector =
     open FEval.EvaluationTypes
     open FEval.EvaluationEvents
     open FEval.Inspections.CommonInspections
+    open FEval.Inspections.Logging
     open FEval.Inspections.TypeFormatters
+    open System
 
     type InspectionResult =
         {
+            Time : DateTime 
             Name : string
             Value : string
         }
 
     // Private functions
 
-    let private inspectSetVariable eventDetails =
+    let private inspectSetVariable eventDetails time =
         { 
+            Time = time
             Name = eventDetails.Variable.Name
             Value = formatValue eventDetails.Value eventDetails.Variable.Type
         }
@@ -29,8 +33,9 @@ module DataSetInspector =
                                 <| eventDetails.IndexerParameters
         sprintf "%s%s" propertyName indexerParameters
                
-    let private inspectSetProperty eventDetails =
+    let private inspectSetProperty eventDetails time =
         { 
+            Time = time
             Name = formatSetPropertyName eventDetails
             Value = formatValue eventDetails.Value eventDetails.Property.PropertyType
         }
@@ -39,22 +44,47 @@ module DataSetInspector =
         getInstanceType eventDetails.Instance eventDetails.Field.DeclaringType
         |> formatFieldName eventDetails.Field
 
-    let private inspectSetField eventDetails =
+    let private inspectSetField eventDetails time =
         { 
+            Time = time
             Name = formatSetFieldName eventDetails
             Value = formatValue eventDetails.Value eventDetails.Field.FieldType
         }
 
     let private handleInspectionMessage logAction message =
         match message with
-        | IsPreInspection _ & IsSetVariableEvent eventDetails
-            -> logAction <| inspectSetVariable eventDetails
-        | IsPreInspection _ & IsSetPropertyEvent eventDetails
-            -> logAction <| inspectSetProperty eventDetails
-        | IsPreInspection _ & IsSetFieldEvent eventDetails
-            -> logAction <| inspectSetField eventDetails
+        | IsPreInspection inspectionContext & IsSetVariableEvent eventDetails
+            -> logAction <| inspectSetVariable eventDetails inspectionContext.Time
+        | IsPreInspection inspectionContext & IsSetPropertyEvent eventDetails
+            -> logAction <| inspectSetProperty eventDetails inspectionContext.Time
+        | IsPreInspection inspectionContext & IsSetFieldEvent eventDetails
+            -> logAction <| inspectSetField eventDetails inspectionContext.Time
         | _ -> ignore()
     
     // Public functions
         
     let createNew logAction = createInspector <| handleInspectionMessage logAction
+
+    let stringInspectionResultFormatter inspectionResult =
+        sprintf "%s - %s <- %s" 
+            <| formatDateTimeForLog inspectionResult.Time 
+            <| inspectionResult.Name 
+            <| inspectionResult.Value
+            
+    let csvInspectionResultFormatter inspectionResult =
+        sprintf "%s,%s,\"%s\""
+            <| formatDateTimeForLog inspectionResult.Time 
+            <| inspectionResult.Name 
+            <| inspectionResult.Value
+
+    let defaultLogConfig =
+        {
+            Formatter = stringInspectionResultFormatter
+            Header = None
+        }
+
+    let csvLogConfig =
+        {
+            Formatter = csvInspectionResultFormatter
+            Header = Some "Time,Name,Value"
+        }
