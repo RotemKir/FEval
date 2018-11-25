1b003ef167ce114795120c74706fe6ac19377e1e
Rotem Kirshenbaum <rotemkir@gmail.com>
2018-11-25 23:02:53 +0200

    Initial implementation of validation module

---
+++ FEval/Inspections/ValidationInspector.fs
@@ -0,0 +1,27 @@
+﻿namespace FEval.Inspections
+
+[<RequireQualifiedAccess>]
+module Valudate =
+    open FEval.EvaluationTypes
+    
+    type Rule = InspectionContext -> Result<unit, string>
+
+    type RuleTarget =
+        | Const of obj
+        | Variable of name : string
+
+    type Test =
+        | IsNotZero
+        | IsNotNegative
+        | IsNotEmpty
+        | IsLessThan of RuleTarget
+        | IsMoreThan of RuleTarget
+
+    type RuleDefinition =
+        | Variable of name : string * test : Test
+        | Custom of Rule
+
+    let private runRule definition inspectionContext =
+        match definition with
+        | Variable (name, test) -> Result.Ok()
+        | Custom rule           -> rule inspectionContext
