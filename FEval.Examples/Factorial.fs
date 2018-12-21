a9cf921356c5faff24185e15a758c6a24ed39c1c
Rotem Kirshenbaum <rotemkir@gmail.com>
2018-12-21 12:24:41 +0200

    Added factorial examples

---
+++ FEval.Examples/Factorial.fs
@@ -0,0 +1,37 @@
+﻿namespace FEval.Examples
+
+open FEval.Evaluations
+open FEval.Inspections
+open System
+
+module Factorial =
+
+    let private factorialExpr number = 
+        <@ 
+            let mutable x = 1
+
+            for i = 1 to number do
+                x <- x * i
+
+            x
+        @>
+    
+    let private runFactorialWithInspection inspector =
+        Console.WriteLine("Enter a number to calculate factorial for:")
+        let number = Int32.Parse <| Console.ReadLine()
+        let result = 
+            evalWith 
+                <| factorialExpr number
+                <| [| inspector |]
+        Console.WriteLine("Result is: {0}", result)
+        Console.WriteLine("Press any key to continue")
+        Console.Read() |> ignore
+        Console.ReadLine() |> ignore
+
+    let runFactorialWithPerformance() =
+        PerformanceInspector.createNew <| PerformanceInspector.createTxtLogger @"Logs\Perfromance.txt"
+        |> runFactorialWithInspection
+
+    let runFactorialWithDataSet() =
+        DataSetInspector.createNew <| DataSetInspector.createTxtLogger @"Logs\DataSet.txt"
+        |> runFactorialWithInspection
--- FEval.Examples/Program.fs
+++ FEval.Examples/Program.fs
@@ -1,27 +1,38 @@
 ﻿namespace FEval.Examples
 
 open System
+open Factorial
+open System.IO
 
 module Main =
     
     let private showMenu() =
         Console.Clear()
-        Console.WriteLine("Enter the number of the option to run:")
-        Console.WriteLine("1. Factorial")
+        Console.WriteLine("Enter the number of the option to run, any other key will exit:")
+        Console.WriteLine("1. Factorial with performance inspection")
+        Console.WriteLine("2. Factorial with data set inspection")
         Console.WriteLine()
-        Console.WriteLine("Any other key will exit.")
 
     let private getOptionToRun optionValue =
         match optionValue with
-        | "1" -> Some <| fun () -> Console.WriteLine("Factorial") ; Console.ReadLine() |> ignore
+        | "1" -> Some <| runFactorialWithPerformance
+        | "2" -> Some <| runFactorialWithDataSet
         | _   -> None
 
+    let private createLogFolder() =
+        match Directory.Exists("Logs") with
+        | false -> Directory.CreateDirectory("Logs") |> ignore
+        | true  -> ignore()
+
     let rec private run() =
+        createLogFolder()
         showMenu()
 
         match Console.ReadLine() |> getOptionToRun with
-        | Some option -> option() ; run()
-        | None        -> ignore()
+        | Some option -> 
+            option()
+            run()
+        | None -> ignore()
 
     [<EntryPoint>]
     let main _ = 
