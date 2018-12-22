2ca0b48c259ef1573e6ce4bc352d3245e0118687
Rotem Kirshenbaum <rotemkir@gmail.com>
2018-12-22 15:37:55 +0200

    Added method call inspection examples

--- FEval.Examples/Factorial.fs
+++ FEval.Examples/Factorial.fs
@@ -15,22 +15,42 @@ module Factorial =
 
             x
         @>
-    
-    let private runFactorialWithInspection inspector =
+
+    let private runFactorialWithInspections inspections =
         Console.WriteLine("Enter a number to calculate factorial for:")
         let number = Int32.Parse <| Console.ReadLine()
         let result = 
             evalWith 
                 <| factorialExpr number
-                <| [| inspector |]
+                <| inspections
         Console.WriteLine("Result is: {0}", result)
         Console.WriteLine("Press any key to continue")
         Console.ReadKey() |> ignore
 
+    
+    let private runFactorialWithInspection inspector =
+        runFactorialWithInspections [| inspector |]
+
     let runFactorialWithPerformance() =
-        PerformanceInspector.createNew <| PerformanceInspector.createTxtLogger @"Logs\Perfromance.txt"
+        PerformanceInspector.createNew 
+            <| PerformanceInspector.createTxtLogger @"Logs\Perfromance.txt"
         |> runFactorialWithInspection
 
     let runFactorialWithSetValue() =
-        SetValueInspector.createNew <| SetValueInspector.createTxtLogger @"Logs\SetValue.txt"
+        SetValueInspector.createNew 
+            <| SetValueInspector.createTxtLogger @"Logs\SetValue.txt"
         |> runFactorialWithInspection
+
+    let runFactorialWithMethodCall() =
+        MethodCallInspector.createNew 
+            <| MethodCallInspector.createTxtLogger @"Logs\MethodCall.txt"
+        |> runFactorialWithInspection
+
+    let runFactorialWithMethodCallAndSetValue() =
+        [| 
+            MethodCallInspector.createNew 
+                <| MethodCallInspector.createTxtLogger @"Logs\MethodCallAndSetValue.txt"
+            SetValueInspector.createNew 
+                <| SetValueInspector.createTxtLogger @"Logs\MethodCallAndSetValue.txt"
+        |]
+        |> runFactorialWithInspections
--- FEval.Examples/Program.fs
+++ FEval.Examples/Program.fs
@@ -11,12 +11,16 @@ module Main =
         Console.WriteLine("Enter the number of the option to run, any other key will exit:")
         Console.WriteLine("1. Factorial with performance inspection")
         Console.WriteLine("2. Factorial with set value inspection")
+        Console.WriteLine("3. Factorial with method call inspection")
+        Console.WriteLine("4. Factorial with method call and set value inspection")
         Console.WriteLine()
 
     let private getOptionToRun optionValue =
         match optionValue with
         | "1" -> Some <| runFactorialWithPerformance
         | "2" -> Some <| runFactorialWithSetValue
+        | "3" -> Some <| runFactorialWithMethodCall
+        | "4" -> Some <| runFactorialWithMethodCallAndSetValue
         | _   -> None
 
     let private createLogFolder() =
